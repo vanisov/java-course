6031850da01a8b64ff8bb4af14b62bc96da76770
Vlad Anisov <100395661+vvanii@users.noreply.github.com>
2023-02-21 01:24:52 -0800

    finished couple segments, left off on gui intro

--- expressions/src/expressions/Main.java
+++ expressions/src/expressions/Main.java
@@ -3,8 +3,14 @@ package expressions;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		double friends = 10;
+		
+		friends = friends / 3;
+		
+		System.out.println(friends);
+		
 	}
 
 }
---
+++ gui_intro/src/gui_intro/Main.java
@@ -0,0 +1,11 @@
+package gui_intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// TODO: Time stamp = 49:58
+		
+	}
+
+}
